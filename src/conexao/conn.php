1c1774a1b4fba3b0bcda9f0b87020ed8b5747921
anavitoriambb <ana.barbosa217@etec.sp.gov.br>
2021-06-17 18:06:18 -0300

    commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,10 +1,15 @@
 <?php
 
     // Declarar as variáveis necessárias para gerar a minha conexão com o banco de dados
-    $hostname = "fdb18.awardspace.net";
-    $dbname = "3838903_library";
-    $username = "3838903_library";
-    $password = "Et3cL1br@ry";
+    //$hostname = "fdb18.awardspace.net";
+    //$dbname = "3838903_library";
+    //$username = "3838903_library";
+    //$password = "Et3cL1br@ry";
+
+    $hostname = "sql213.epizy.com";
+    $dbname = "epiz_28849729_library";
+    $username = "epiz_28849729";
+    $password = "nVPiBfmcawbw";
 
     try {
         $pdo = new PDO('mysql:host=' .$hostname.';dbname='.$dbname, $username, $password);
